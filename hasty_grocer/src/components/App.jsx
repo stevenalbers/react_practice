b5a2cc915c2d4a8b07fbebf912396900227f85db
Steven Albers <stevenalbers@gmail.com>
2018-08-22 21:29:10 -0700

    Cloned Hasty Grocer project from Goal Coach

---
+++ hasty_grocer/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { firebaseApp } from '../firebase';
+
+
+class App extends Component {
+
+	signOut() {
+		firebaseApp.auth().signOut();
+	}
+	render() {
+		return (
+			<div>app
+				<button 
+				className="btn btn-danger" 
+				onClick={() => this.signOut()}>
+					Sign Out
+				</button>
+			</div>
+		)
+	}
+}
+
+function mapStateToProps(state) {
+	console.log('state', state);
+	return {}
+}
+
+
+export default connect(mapStateToProps, null)(App);
---
+++ hasty_grocer/src/firebase.js
@@ -0,0 +1,8 @@
+import * as firebase from 'firebase';
+
+  // Initialize Firebase
+  const config = {
+    // Import config from firebase
+  };
+
+  export const firebaseApp = firebase.initializeApp(config);
